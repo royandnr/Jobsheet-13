4f4082f3e60f767f5d8ddbd07403c03ef35a86e3
Yann <yann@gmail.com>
2025-12-24 17:22:44 +0700

    Percobaan 3

---
+++ Percobaan3.java
@@ -0,0 +1,29 @@
+    import java.util.Scanner;
+
+    public class Percobaan3 {
+        
+        static double HitungLaba (double saldo, int tahun) {
+            if (tahun == 0) {
+                return (saldo);
+            } else {
+                return (1.11 * HitungLaba(saldo, tahun - 1));
+            } 
+        }
+        
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            
+            double saldoAwal;
+            int tahun;
+
+            System.out.print("Jumlah saldo awal: ");
+            saldoAwal = sc.nextDouble();
+            System.out.print("Lamanya investasi (tahun): ");
+            tahun = sc.nextInt();
+
+            System.out.print("Jumlah saldo setelah " + tahun + " tahun: ");
+                System.out.printf("%.2f", HitungLaba(saldoAwal, tahun));
+        
+            }
+        }
+        
